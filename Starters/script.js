d846a021a0b6c2df4428959da6cbfd5ddc68d7a4
gh4abhi <gh4abhi@gmail.com>
2024-05-02 02:42:58 +0530

    Added starter code.

---
+++ Starters/script.js
@@ -0,0 +1,27 @@
+let authName = "gh4abhi";
+let PI = 3.1415;
+let firstName = 'abhi';
+let lastName = 'shri';
+console.log(firstName + ' ' + lastName);
+console.log(authName);
+console.log(typeof authName);
+
+function fn(fname) {
+    console.log(fname);
+}
+
+const ln = function (lname) {
+    console.log(lname);
+}
+
+const rl = (role) => role;
+
+const obj = {
+    fname: "Genius",
+    lname: "gh4abhi",
+    role: "Devloper"
+}
+
+fn(obj.fname);
+ln(obj['lname']);
+console.log(rl(obj.role));
